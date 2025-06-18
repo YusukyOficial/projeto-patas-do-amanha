08a111ec25f77fe73f298fc18a4bf82b76b33a84
MATHEUS GONCALVES <matheus.goncalves12@senacsp.edu.br>
2025-06-18 19:28:19 -0300

    att-develop

--- src/pages/Weslley.jsx
+++
@@ -1,7 +0,0 @@
-function Weslley (){
-    return (<p>
-        Page Weslley
-    </p>)
-}
-
-export default Weslley;
--- src/pages/taruma.jsx
+++
@@ -1,7 +0,0 @@
-function taruma(){
-    return(
-        <p>
-         
-        </p>
-    )
-}
